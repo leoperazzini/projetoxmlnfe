35224ef8ec4a2da8b20121a91c50a11d478ade07
leoperazzini <leoperazzini@hotmail.com>
2019-02-04 23:25:05 -0200

    4

--- resources/views/dependencias.blade.php
+++ resources/views/dependencias.blade.php
@@ -5,3 +5,11 @@
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
 
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+
+                <link href="{{ URL::to('/') }}/css/bootstrap-datepicker.css" rel="stylesheet"/>
+        <link href="{{ URL::to('/') }}/css/bootstrap.min.css" rel="stylesheet"/>
+        <link href="{{ URL::to('/') }}/css/bootstrap.css" rel="stylesheet"/> 
+        <link href="{{ URL::to('/') }}/css/maps.css" rel="stylesheet"/> 
+ 
+        <script src="{{ URL::to('/') }}/js/bootstrap-datepicker.min.js"></script> 
+        <script src="{{ URL::to('/') }}/js/bootstrap-datepicker.pt-BR.min.js" charset="UTF-8"></script> 
